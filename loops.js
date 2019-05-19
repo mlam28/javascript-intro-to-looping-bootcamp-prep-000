dc4cb663cdae1918772f9030e99d90c9f438fea3
Michelle Lam <shelly2.lam@gmail.com>
2019-05-19 02:32:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,9 +5,4 @@ function forLoop(array) {
    return array;
 }
 
-function maybeTrue() {
-    return Math.random() >= 0.5}
-    
-function whileLoop(n) {
-  while (n > 0) {console.log(--n);
-  return "done"}}
+
