cb9a6afe6a952a7464573da19a2218980d18a2e7
Michelle Lam <shelly2.lam@gmail.com>
2019-05-19 03:10:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,9 +10,13 @@ function whileLoop(n) {
   return "done"
 }
 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
 
 function doWhileLoop(i) {
   do {console.log('I run once regardless.');
 }
-  while (incrementVariable  < 5)
+  while (incrementVariable()  < 5)
   }
