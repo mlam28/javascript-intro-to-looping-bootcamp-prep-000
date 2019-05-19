1216c4616d4f6dc50235daed952d3c124eadb29f
Michelle Lam <shelly2.lam@gmail.com>
2019-05-19 01:51:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array){
   if (var i = 1) {
-    return `I am ${i} strange loop`;}
+    console.log(`I am ${i} strange loop`)};
     else{
-  for([var i = 0]; [i < 25]; [i++]) {
+  for(var i = 0; i < 25; i++) {
     console.log(`I am ${i} strange loops.`)
   }
   return array
-}
+} }
